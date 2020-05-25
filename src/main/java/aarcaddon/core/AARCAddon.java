629e86489c7efd9fc0cd7a4608a487b1b3e76e8b
fonnymunkey <fonnymunkey@gmail.com>
2020-05-25 02:02:45 -0400

    Update AARCAddon.java
    Fixed modloading issue

--- src/main/java/aarcaddon/core/AARCAddon.java
+++ src/main/java/aarcaddon/core/AARCAddon.java
@@ -8,12 +8,12 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 import aarcaddon.core.proxies.CommonProxy;
 
-@Mod(modid = AARCAddon.MODID, name = AARCAddon.NAME, version = AARCAddon.VERSION)
+@Mod(modid = AARCAddon.MODID, name = AARCAddon.NAME, version = AARCAddon.VERSION, dependencies = "after:antiqueatlas;after:reccomplex")
 public class AARCAddon
 {
     public static final String MODID = "aarcaddon";
     public static final String NAME = "AARCAddon";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     
     @SidedProxy(clientSide = "aarcaddon.core.proxies.ClientProxy", serverSide = "aarcaddon.core.proxies.CommonProxy")
     public static CommonProxy proxy;
